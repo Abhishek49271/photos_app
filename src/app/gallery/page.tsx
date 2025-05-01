b602e17f0e02e576782f86f69a18b4753cd82d9e
DhruvGoyal <122073893+Dhruv0606@users.noreply.github.com>
2025-05-02 00:02:29 +0530

    Added Cloudinary setup with working upload

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,46 @@
+
+import UploadButton from "./upload-button";
+import cloudinary from "cloudinary";
+import GalleryGrid from "./gallery-grid";
+
+export type SearchResult ={
+  public_id: string;
+  tags: string[];
+};
+
+export default async function GalleryPage() {
+
+  const results = (await cloudinary.v2.search
+    .expression("resource_type:image")
+    .sort_by("created_at","desc")
+    .with_field("tags")
+    .max_results(30)
+    .execute()) as{ resources: SearchResult[]};
+
+  // console.log("results", results)
+
+  // const MAX_COLUMNS = 4;
+
+
+  // function getColumns(colIndex: number){
+  //   return results.resources.filter((resource, idx) => 
+  //      idx % MAX_COLUMNS === colIndex
+  //   )
+
+  // }
+
+  return (
+    <section>
+      <div className="flex flex-col gap-8">
+        <div className="flex justify-between">
+          <h1 className="text-4xl font-bold">Gallery</h1>
+          <UploadButton />
+        </div>
+
+      <GalleryGrid
+      images={results.resources}
+      />
+      </div>
+    </section>
+  );
+}
